73c3911d8784f2b98c9900a858b93261e578f199
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-03-30 20:02:08 -0400

    update

---
+++ items.js
@@ -0,0 +1,14 @@
+// GET /items - this should render a list of shopping items.
+// Here is what a response looks like:
+
+// [{“name”: “popsicle”, “price”: 1.45}, {“name”:”cheerios”, “price”: 3.40}]
+
+/** GET / => [item, ...] */
+
+router.get('', (req, res, next) => {
+    try {
+      return res.json({ items: Item.findAll() });
+    } catch(err){
+      return next(err)
+    }
+  });
