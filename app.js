763bd47569a453298a743b84e9c29a1c11f33119
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-03-30 21:06:16 -0400

    update

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
- const Express = require('express')
+ const express = require('express')
  const app = express();
+ const itemsRoutes = require("./items")
 
- 
+/** general error handler */
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+  
+    return res.json({
+      error: err.message,
+    });
+  });
+
+
+ module.exports = app
