77047d12a6f6d1f811df36ff47dd7c233f7bd40a
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-01 13:08:40 -0400

    router.get("/")
    gets list of all the companies

--- Routes/companies.js
+++ Routes/companies.js
@@ -5,3 +5,44 @@ const ExpressError = require("../expressError")
 const db = require("../db");
 
 let router = new express.Router();
+
+
+/**Step 2: Add Company Routes
+Create routes/companies.js with a router in it.
+
+All routes in this file should be found under companies/.
+
+All routes here will respond with JSON responses. These responses will be in an object format where the value is the data from the database.
+
+So, for example, the “get list of companies should return”:
+
+{companies: [{code, name}, ...]}
+Assuming result is the result from your query, you could produce this with a line like:
+
+return res.json({companies: result.rows})
+These routes need to be given data in JSON format, not the standard “url-encoded form body” — so you’ll need to make sure that your app.js includes the middleware to parse JSON.
+
+Routes Needed
+GET /companies
+Returns list of companies, like {companies: [{code, name}, ...]} */
+
+/** GET / => list of companies.
+ *
+ * =>  {companies: [{code, name, descrip}, {code, name, descrip}, ...]}
+ *
+ * */
+ router.get("/", async function (req, res, next) {
+    try {
+      const result = await db.query(
+            `SELECT code, name 
+             FROM companies 
+             ORDER BY name`
+      );
+  
+      return res.json({"companies": result.rows});
+    }
+  
+    catch (err) {
+      return next(err);
+    }
+  });
