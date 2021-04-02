cd3d2281407131d6aa50ecb46ed4fdfae7606a8f
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-02 14:41:46 -0400

    Put update company, Delete company

--- Routes/companies.js
+++ Routes/companies.js
@@ -118,4 +118,64 @@ router.post("/", async function (req, res, next) {
   }
 });
 
+/** PUT /[code] => update company
+ *
+ * {name, descrip}  =>  {company: {code, name, descrip}}
+ *
+ * */
+
+ router.put("/:code", async function (req, res, next) {
+  try {
+    let {name, description} = req.body;
+    let code = req.params.code;
+
+    const result = await db.query(
+          `UPDATE companies
+           SET name=$1, description=$2
+           WHERE code = $3
+           RETURNING code, name, description`,
+        [name, description, code]);
+
+    if (result.rows.length === 0) {
+      throw new ExpressError(`No such company: ${code}`, 404)
+    } else {
+      return res.json({"company": result.rows[0]});
+    }
+  }
+
+  catch (err) {
+    return next(err);
+  }
+
+});
+
+
+/** DELETE /[code] => delete company
+ *
+ * => {status: "added"}
+ *
+ */
+
+router.delete("/:code", async function (req, res, next) {
+  try {
+    let code = req.params.code;
+
+    const result = await db.query(
+          `DELETE FROM companies
+           WHERE code=$1
+           RETURNING code`,
+        [code]);
+
+    if (result.rows.length == 0) {
+      throw new ExpressError(`No such company: ${code}`, 404)
+    } else {
+      return res.json({"status": "deleted"});
+    }
+  }
+
+  catch (err) {
+    return next(err);
+  }
+});
+
   module.exports = router;
